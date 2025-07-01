111a067136217e66d71b840bec511937e60db06b
lilianlacerda <lilianraiany@gmail.com>
2025-06-30 21:51:18 -0300

    primeiro commit

---
+++ src/test/java/com/example/lilisolution/cadastro_pet/CadastroPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lilisolution.cadastro_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
