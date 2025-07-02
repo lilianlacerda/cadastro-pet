c166b911d465c72f986c3b507325551381d31e64
lilianlacerda <lilianraiany@gmail.com>
2025-07-01 21:33:04 -0300

    add: classe service regra de negocio

---
+++ src/main/java/com/example/lilisolution/cadastro_pet/service/CadastroPetService.java
@@ -0,0 +1,37 @@
+package com.example.lilisolution.cadastro_pet.service;
+
+import com.example.lilisolution.cadastro_pet.repository.CadastroPetRepository;
+import com.example.lilisolution.cadastro_pet.entity.CadastroPet;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+
+public class CadastroPetService {
+    private CadastroPetRepository cadastroPetRepository;
+
+    // Injeção de dependência do repositório
+    public CadastroPetService(CadastroPetRepository cadastroPetRepository) {
+        this.cadastroPetRepository = cadastroPetRepository;
+    }
+
+    public List<CadastroPet> listarPets(){
+        Sort sort = Sort.by("porte").descending().and(Sort.by("nome").ascending());
+        return cadastroPetRepository.findAll(sort);
+    }
+
+    public List<CadastroPet> cadastrarPet(CadastroPet cadastroPet) {
+        cadastroPetRepository.save(cadastroPet);
+        return listarPets();
+    }
+
+    public List<CadastroPet> atualizarPet(CadastroPet cadastroPet){
+        cadastroPetRepository.save(cadastroPet);
+        return listarPets();
+    }
+
+    public List<CadastroPet> excluirPet(Long id){
+        cadastroPetRepository.deleteById(id);
+        return listarPets();
+    }
+    
+}
